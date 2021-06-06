4d3e78a0cb7a6bf1cd9b047b3c36f7ca178e5072
maria-okoledova394 <maria-okoledova@yandex.ru>
2021-06-07 00:25:21 +0300

    Rename img in camelCase

--- src/components/InfoTooltip.js
+++ src/components/InfoTooltip.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import success_registration_mark from '../images/success_registration_mark.svg'
-import faild_registration_mark from '../images/faild_registration_mark.svg'
+import successRegistrationMark from '../images/successRegistrationMark.svg'
+import faildRegistrationMark from '../images/faildRegistrationMark.svg'
 
 function InfoTooltip(props) {
     return (
@@ -8,7 +8,7 @@ function InfoTooltip(props) {
         <div className={`popup ${props.isOpen ? "popup_opened" : ""}`}>
           <div className="popup__container popup__container_type_info">
             <button className="popup__close-button" type="button" onClick={props.onClose}></button>
-            <img className="popup__image" src={props.isMistake ? faild_registration_mark : success_registration_mark}
+            <img className="popup__image" src={props.isMistake ? faildRegistrationMark : successRegistrationMark}
             alt="mark"></img>
             <h2 className="popup__title popup__title_function_info">
               {props.isMistake ? "Что-то пошло не так! Попробуйте ещё раз." : "Вы успешно зарегистрировались!"}</h2>
